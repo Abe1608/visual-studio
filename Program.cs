661bd8dea9e59e19a26db199d6c171494f9e8f60
GUTS <GUTS@DESKTOP-QR93HO7>
2025-06-21 22:04:07 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Ejercicio 1 
+/* Da la bienvenida a la UTEC a un usuario preguntale su nombre, su edad, 
+ * que materias cursa y como siente su experiencia de estudios en la Universidad. 
+ * (convierte la edad a entero).Luego muestra en consola los datos solicitados:
+ */
+
+//Solución
+
+
+
