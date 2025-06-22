f0d7dd0e6e3e8c5b4f2d80423fc3ae6199c181f0
GUTS <GUTS@DESKTOP-QR93HO7>
2025-06-22 17:11:31 -0600

    Actualizacion de comentarios

--- Program.cs
+++ Program.cs
@@ -6,29 +6,28 @@
 
 //Solución
 
-
-string nombreAlumno = "";  // Almacenara el nombre del usuario
-int edadAlumno = 0;    // se guardara como un numero entero 
-string asignatura = ""; // esta variable va a almacenar la materia o asignatura que el usuario esta cursando
-string comentario = ""; // Esta variable almacenara el comentario del usuario acerca de su experiencia en la universidad
+string nombreAlumno = "";  // Almacenara el nombre del usuario Author: 17-2940-2020, Abelardo Amaya
+int edadAlumno = 0;    // se guardara como un numero entero Author: 17-2940-2020, Abelardo Amaya
+string asignatura = ""; // esta variable va a almacenar la materia o asignatura que el usuario esta cursando Author: 17-2940-2020, Abelardo Amaya
+string comentario = ""; // Esta variable almacenara el comentario del usuario acerca de su experiencia en la universidad Author: 17-2940-2020, Abelardo Amaya
 
 //Saludo
-Console.WriteLine(" Bienvenido a la Universidad Tecnologica de El Salavador (UTEC) ");
-Console.WriteLine("*************************************************");
-Console.WriteLine("¡Por favor! ingrese su nombre para identificarlo:");
-nombreAlumno = Console.ReadLine(); // Se solicita la insercion de nombre al usuario y lo almacena en la variable nombre que habiamos inicializado
-Console.WriteLine(" Ingresa tu edad en numeros enteros:");
-edadAlumno = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese la materia que esta cursando:");
-asignatura = Console.ReadLine(); // Se solicita insercion de la asignatura del usuario
-Console.WriteLine("Ahora cuentenos ¿Como ha sido su experiencia desde que ingreso a la Universidad?");
-comentario = Console.ReadLine(); // Se solicita insercion de una breve explicación 
+Console.WriteLine(" Bienvenido a la Universidad Tecnologica de El Salavador (UTEC) "); //Author: 17-2940-2020, Abelardo Amaya
+Console.WriteLine("*************************************************"); //Author: 17-2940-2020, Abelardo Amaya
+Console.WriteLine("¡Por favor! ingrese su nombre para identificarlo:"); // Author: 17-2940-2020, Abelardo Amaya
+nombreAlumno = Console.ReadLine(); // Se solicita la insercion de nombre al usuario y lo almacena en la variable nombre que habiamos inicializado Author: 17-2940-2020, Abelardo Amaya
+Console.WriteLine(" Ingresa tu edad en numeros enteros:"); // Author: 17 - 2940 - 2020, Abelardo Amaya
+edadAlumno = int.Parse(Console.ReadLine()); //Author: 17-2940-2020, Abelardo Amaya
+Console.WriteLine("Ingrese la materia que esta cursando:") //Author: 17-2940-2020, Abelardo Amaya
+asignatura = Console.ReadLine(); // Se solicita insercion de la asignatura del usuario Author: 17-2940-2020, Abelardo Amaya
+Console.WriteLine("Ahora cuentenos ¿Como ha sido su experiencia desde que ingreso a la Universidad?"); //Author: 17-2940-2020, Abelardo Amaya
+comentario = Console.ReadLine(); // Se solicita insercion de una breve explicación Author: 17-2940-2020, Abelardo Amaya
 
-// impresion con interpolación
+// impresion con interpolación Author: 17-2940-2020, Abelardo Amaya
 
-Console.WriteLine($"\nEstudiante ingresado:\nNombre: {nombreAlumno}" +
-    $"\nEdad: {edadAlumno} años\nMateria cursada: {asignatura}" +
-    $"\nOpinion acerca de su experiencia en la U: {comentario}");
+Console.WriteLine($"\nEstudiante ingresado:\nNombre: {nombreAlumno}" + // Author: 17-2940-2020, Abelardo Amaya
+    $"\nEdad: {edadAlumno} años\nMateria cursada: {asignatura}" + // Author: 17-2940-2020, Abelardo Amaya
+    $"\nOpinion acerca de su experiencia en la U: {comentario}"); // Author: 17-2940-2020, Abelardo Amaya
 
 
 
